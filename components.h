ea9634c6c45b71ad1259efa70b1451d768d8e8ca
Josiah Beals <jdbeals95@gmail.com>
2025-08-07 01:16:54 -0400

    CHORE: add components.h, entity_factory.h

--- components.h
+++ components.h
@@ -4,5 +4,17 @@
 
 #ifndef SPARSE_STORAGE_LEARNING_COMPONENTS_H
 #define SPARSE_STORAGE_LEARNING_COMPONENTS_H
+#include <stdbool.h>
+
+typedef  struct StatComponent {
+    int health;
+    int attack_level;
+    int defense_level;
+} stat_component;
+
+typedef struct CombatComponent {
+    bool is_attacking;
+    bool is_defending;
+} combat_component;
 
 #endif //SPARSE_STORAGE_LEARNING_COMPONENTS_H
