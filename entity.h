405d99a76261775804a35924c56495f82b209c48
Josiah Beals <jdbeals95@gmail.com>
2025-08-07 01:03:54 -0400

    Initial commit

---
+++ entity.h
@@ -0,0 +1,15 @@
+//
+// Created by jo on 8/7/2025.
+//
+
+#ifndef SPARSE_STORAGE_LEARNING_ENTITY_H
+#define SPARSE_STORAGE_LEARNING_ENTITY_H
+
+#include <stdint.h>
+
+typedef struct Entity{
+    uint32_t entity_id;
+    uint32_t entity_version;
+} entity;
+
+#endif //SPARSE_STORAGE_LEARNING_ENTITY_H
---
+++ sparse_set_storage.c
@@ -0,0 +1,9 @@
+
+typedef struct SparseStorage {
+    int* sparse_storage;
+    int* dense_storage;
+    int active_components;
+} sparse_storage;
+
+// Created by jo on 8/6/2025.
+//
